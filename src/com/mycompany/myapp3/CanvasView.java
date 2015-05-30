49467cfc4930d219dc0c15d664fd8ad6cdd65962
DocRandom <r.einhardt@icloud.com>
2015-05-30 11:37:51 +0200

    branchCommitRudi

--- src/com/mycompany/myapp3/CanvasView.java
+++ src/com/mycompany/myapp3/CanvasView.java
@@ -5,6 +5,8 @@ import android.graphics.*;
 import javax.microedition.*;
 public class CanvasView extends View{
 	private Paint myPaint;
+	Paint rudi;
+	RectF ovalA=new RectF();
 
 public CanvasView(Context context){
 	super(context);
@@ -13,10 +15,14 @@ public CanvasView(Context context){
 public void setPaintAttributes(){
 	myPaint = new Paint();
 	myPaint.setColor(Color.GREEN);
+	rudi = new Paint();
+	rudi.setColor(Color.parseColor("#a23957"));
 	}
 	@Override
 	public void onDraw(Canvas canvas){
 		canvas.drawColor(Color.parseColor("#a18343"));
-		canvas.drawCircle(153.0f,325.0f,55.0f,myPaint);
+		canvas.drawCircle(253.0f,325.0f,55.0f,myPaint);
+		ovalA.set(20,20,200,450);
+		canvas.drawArc(ovalA,36,142,true,rudi);
 	}
 }
